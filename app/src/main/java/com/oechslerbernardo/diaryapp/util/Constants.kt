71c1d6ba4972c81c2f6388244b44f9bc27b69d8a
nardober1st <bernardo.oechsler6@gmail.com>
2023-12-08 00:23:35 -0800

    mongo db const added

---
+++ app/src/main/java/com/oechslerbernardo/diaryapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.oechslerbernardo.diaryapp.util
+
+object Constants {
+
+    const val APP_ID = "mydiaryapp-gafdd"
+    const val CLIENT_ID = "1036158125468-5e5ob2r5jpk76s3mbscrp4inhtpa527r.apps.googleusercontent.com"
+}
